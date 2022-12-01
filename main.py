e2c74916df9551bc1c67270434521b4ed4a6a552
Александр Лапп <rogty20@yandex.ru>
2022-12-01 12:32:53 +0300

    fixed yandex_practicum

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str):
+    """Важно лишь одно: чтобы докстринги были оформлены по правилам."""
     if char_class == 'warrior':
         return f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}'
     if char_class == 'mage':
@@ -11,7 +13,8 @@ def attack(char_name, char_class):
                f'{5 + randint(-3, -1)}'
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str):
+    """Важно лишь одно: чтобы докстринги были оформлены по правилам."""
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
@@ -20,7 +23,8 @@ def defence(char_name, char_class):
         return f'{char_name} блокировал {10 + randint(2, 5)} урона'
 
 
-def special(char_name, char_class):
+def special(char_name: str, char_class: str):
+    """Важно лишь одно: чтобы докстринги были оформлены по правилам."""
     if char_class == 'warrior':
         return f'{char_name} применил специальное умение «Выносливость ' \
                f'{80 + 25}»'
@@ -30,7 +34,8 @@ def special(char_name, char_class):
         return f'{char_name} применил специальное умение «Защита {10 + 30}»'
 
 
-def start_training(char_name, char_class):
+def start_training(char_name: str, char_class: str):
+    """Важно лишь одно: чтобы докстринги были оформлены по правилам."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -54,11 +59,12 @@ def start_training(char_name, char_class):
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    """Важно лишь одно: чтобы докстринги были оформлены по правилам."""
+    approve_choice: str = None
+    char_class: str = None
     while approve_choice != 'y':
-        char_class = input(
+        char_class: str = input(
             'Введи название персонажа, за которого хочешь играть: '
             'Воитель — warrior, Маг — mage, Лекарь — healer: ')
         if char_class == 'warrior':
@@ -70,22 +76,21 @@ def choice_char_class():
         if char_class == 'healer':
             print('Лекарь — могущественный заклинатель. '
                   'Черпает силы из природы, веры и духов.')
-        approve_choice = input(
+        approve_choice: str = input(
             'Нажми (Y), чтобы подтвердить выбор, или '
             'любую другую кнопку, чтобы выбрать другого персонажа ').lower()
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    """Важно лишь одно: чтобы докстринги были оформлены по правилам."""
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
